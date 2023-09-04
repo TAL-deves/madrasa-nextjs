f871ac42124ced019e5b6935724d5b7a896078d4
Istiak26615 <istiak.shishir26615@gmail.com>
2023-09-04 13:14:13 +0600

    build

--- .next/prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"9eb9db14696d2d86b0be2dca6d7bb7c3\",\"previewModeSigningKey\":\"9b64aee39d1ce86b7e907720930cced310c2641387421bb7a8b95967f59078a3\",\"previewModeEncryptionKey\":\"b3de2cb09d7021a1cddec12a5bca795b55c56c09c843a09d5c1d8187dcefaba9\"}}"
